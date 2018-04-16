b7715c1886a8b55e303d19ae129e8f4edec8e918
petterdybdal <petter.dybdal@gmail.com>
2018-04-16 18:19:20 +0200

    Change loading animation

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { PulseLoader } from 'react-spinners';
+import { ScaleLoader } from 'react-spinners';
 import styles from './styles.css';
 
 class Loader extends React.Component {
   render() {
     return (
       <div className={styles.loading}>
-        <PulseLoader color={'#ECB61C'} />
+        <ScaleLoader color={'#ECB61C'} />
       </div>
     );
   }
