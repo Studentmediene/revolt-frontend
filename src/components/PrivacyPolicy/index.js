d46b4f9ec8c6e466edc3ab469cd776ceef833398
Jørgen Henrichsen <jh.henrichs@gmail.com>
2018-09-12 14:34:05 +0200

    Updated the privacy policy.

--- src/components/PrivacyPolicy/index.js
+++ src/components/PrivacyPolicy/index.js
@@ -1,25 +1,24 @@
 import React from 'react';
 
-import { styles } from './styles.css';
-
 const PrivacyPolicy = () => (
   <div>
     <h1>Personvernerklæring</h1>
-    <p>Sist oppdatert: 11. September 2018</p>
+    <p>Sist oppdatert: 12. September 2018</p>
     <p>
-      Denne personvernerklæringen forklarer hvordan Radio Revolt samler inn,
-      behandler og bruker personopplysninger. Erklæringen inneholder
-      opplysninger du har krav på når det samles inn opplysninger fra nettstedet
-      vårt (personopplysningsloven § 19) og generell informasjon om hvordan vi
-      behandler personopplysninger (personopplysningsloven § 18 første ledd).
-      Radio Revolt har ansvar for å følge personopplysningsloven.
+      Denne personvernerklæringen forklarer hvordan Radio Revolt
+      (radiorevolt.no, Radio Revolt på iOS og Radio Revolt på Android) samler
+      inn, behandler og bruker personopplysninger. Erklæringen inneholder
+      opplysninger du har krav på når det samles inn opplysninger fra nettsted
+      og apper (personopplysningsloven § 19) samt generell informasjon om
+      hvordan vi behandler personopplysninger (personopplysningsloven § 18
+      første ledd). Radio Revolt har ansvar for å følge personopplysningsloven.
     </p>
     <h2>Behandlingsansvarlig</h2>
     <p>
       Daglig leder for Studentmediene i Trondheim AS er overordnet
-      behandlingsansvarlig for Radio Revolt. Produkteier for Radio Revolt er
+      behandlingsansvarlig for Radio Revolt. Radioredaktør for Radio Revolt er
       delegert de daglige oppgavene for behandling av personopplysninger.
-      Delegeringen omfatter kun oppgavene og ikke ansvaret. Produkteier kan
+      Delegeringen omfatter kun oppgavene og ikke ansvaret. Radioredaktør kan
       kontaktes på <a href="mailto:redaktor@radiorevolt.no">epost</a>.
     </p>
     <h2>Bruk av informasjonskapsler</h2>
