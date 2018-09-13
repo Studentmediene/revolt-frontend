2b3d3d02e467a426a3f9c650c23a9c146c06e043
Vemund Santi <vemund@santi.no>
2018-09-13 02:41:34 +0200

    style: style HeaderPlayButton to match player colorsy

--- src/components/Player/components/PlayPauseButton.js
+++ src/components/Player/components/PlayPauseButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import styles from './AudioControls.css';
+import styles from './PlayPauseButton.css';
 
 const PlayPauseButton = ({ togglePlayPause, paused }) => (
   <button
