55fb3f6163d2aeebf820f9709bec493e43f71c8a
Vemund Santi <vemund@santi.no>
2018-09-25 11:38:36 +0200

    refactor: remove components and utils from old admin panel project

--- src/components/Categories/tests/reducer.test.js
+++
@@ -1,8 +0,0 @@
-import categoriesReducer from '../reducer';
-import { fromJS } from 'immutable';
-
-describe('categoriesReducer', () => {
-  it('returns the initial state', () => {
-    expect(categoriesReducer(fromJS({}), {})).toEqual(fromJS({}));
-  });
-});
--- src/utils/api.js
+++ src/utils/api.js
@@ -29,16 +29,3 @@ export const getSendeplan = (year, month, date) =>
   fetch(`${SENDEPLAN_API_URL}${year}/${month}/${date}/autoavvikler`)
     .then(handleError)
     .then(res => res.json());
-
-// Lazy way to ignore undefined imports in not yet used files
-export const CATEGORIES_URL = null;
-export const POSTS_URL = null;
-export const EPISODES_URL = null;
-export const SHOWS_URL = null;
-export const PAPPAGORG_SHOWS_URL = null;
-export const post = null;
-export const getDigasOnDemandEpisodes = null;
-export const getDigasPodcastEpisodes = null;
-export const update = null;
-export const apiDelete = null;
-export const getPodcastUrl = null;
