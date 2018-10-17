982253c9d2a094997855251010443b8df8ba970f
Vemund Santi <vemund@santi.no>
2018-10-17 19:30:19 +0200

    feat: make 5-block sections on frontpage posts

--- src/components/PostPreviewList/index.js
+++ src/components/PostPreviewList/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import PostPreview from 'components/PostPreview';
-import styles from './styles.css';
+import styles from './styles.scss';
 
 const PostPreviewList = props => {
   const posts = props.posts.map((post, index) => (
-    <div className={styles.post} key={`post-${index}`}>
+    <div
+      className={classNames({
+        [styles.post]: true,
+        [styles.postLarge]: index % 5 === 0,
+      })}
+      key={index}
+    >
       <PostPreview {...post} />
     </div>
   ));
+
   return <div className={styles.postPreviewList}>{posts}</div>;
 };
 
