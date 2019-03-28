23f8fe206d622f37de39bf4014ad8c4a2e777624
Vemund Santi <vemund@santi.no>
2019-03-28 12:25:29 +0100

    build: update stylelint rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,17 +1,21 @@
 module.exports = {
-  "extends": "stylelint-config-standard",
-  "rules": {
-    "color-hex-case": "upper",
-    "string-quotes": "single",
-    "font-family-name-quotes": "always-where-recommended",
-    "selector-pseudo-class-no-unknown": [
+  extends: 'stylelint-config-standard',
+  rules: {
+    'color-hex-case': 'upper',
+    'string-quotes': 'single',
+    'declaration-empty-line-before': [
+      'never',
+      {
+        ignore: ['after-declaration'],
+      },
+    ],
+    'font-family-name-quotes': 'always-where-recommended',
+    'selector-pseudo-class-no-unknown': [
       true,
       {
-        "ignorePseudoClasses": [
-          "global"
-        ]
-      }
+        ignorePseudoClasses: ['global'],
+      },
     ],
-    "indentation": 2
-  }
-}
+    indentation: 2,
+  },
+};
