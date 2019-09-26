7e5ef393ab0e2d2d81ea67d4cf2a59e3b022736f
Erik Turøy Midtun <etm.midtun@gmail.com>
2019-09-26 19:22:42 +0200

    started on highligted shows component

---
+++ src/components/FrontPage/components/HighlightedShows/index.js
@@ -0,0 +1,15 @@
+import React from 'react';;
+
+import styles from './styles.scss';
+
+class HighlightedShows extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className={styles.container}>Test</div>;
+  }
+}
+
+export default HighlightedShows;
