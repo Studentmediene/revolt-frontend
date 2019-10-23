90bbcd471b87d612483b3ce0bdbad4b7db4befde
Vemund Santi <vemund@santi.no>
2019-10-23 20:28:37 +0200

    fix: force new styles when in development mode

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -43,6 +43,19 @@ if (process.env.NODE_ENV === 'production') {
   }
 }
 
+// Force CSS reloading in dev
+import Router from 'next/router';
+
+Router.events.on('routeChangeComplete', () => {
+  if (process.env.NODE_ENV !== 'production') {
+    const els = document.querySelectorAll(
+      'link[href*="/_next/static/css/styles.chunk.css"]',
+    );
+    const timestamp = new Date().valueOf();
+    els[0].href = '/_next/static/css/styles.chunk.css?v=' + timestamp;
+  }
+});
+
 class RadioRevolt extends App {
   static async getInitialProps({ Component, ctx }) {
     let pageProps = {};
