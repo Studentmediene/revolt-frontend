443e8318ef1958034e468c298905b5de30db2b3f
ingrmj <ingrmj@stud.ntnu.no>
2019-11-03 16:54:31 +0100

    add button for expanding description of episodes

--- src/components/Episode/index.js
+++ src/components/Episode/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
 import { createStructuredSelector } from 'reselect';
+import Expander from './components/Expander';
 
 import styles from './styles.scss';
 import { togglePlayPause } from 'components/Player/actions';
@@ -11,6 +12,7 @@ import PlayPauseButton from 'components/Player/components/PlayPauseButton';
 import { selectEpisodeId, selectPaused } from 'components/Player/selectors';
 
 export class Episode extends React.Component {
+  
   constructor(props) {
     super(props);
 
@@ -18,6 +20,7 @@ export class Episode extends React.Component {
       expanded: false,
     };
   }
+
   static propTypes = {
     digasBroadcastId: PropTypes.number,
     id: PropTypes.number,
@@ -28,6 +31,7 @@ export class Episode extends React.Component {
     playingEpisodeId: PropTypes.number,
     publishAt: PropTypes.string.isRequired,
     paused: PropTypes.bool.isRequired,
+    toggleExpanded: PropTypes.func,
   };
 
   render() {
@@ -43,6 +47,10 @@ export class Episode extends React.Component {
       isCurrentlyPlaying = true;
     }
 
+    const isExpanded = this.state.expanded 
+      ? {display: 'block'}
+      : {display: 'none'};
+
     const playOnDemand = event => {
       event.preventDefault();
       if (this.props.playingEpisodeId === this.props.id) {
@@ -51,6 +59,15 @@ export class Episode extends React.Component {
         this.props.playOnDemand(this.props.id);
       }
     };
+
+    const toggleExpanded = event => {
+      event.preventDefault();
+      event.stopPropagation()
+      this.setState({
+          expanded: !this.state.expanded
+        }
+      )
+    }
 
     return (
       <div
@@ -61,14 +78,14 @@ export class Episode extends React.Component {
         <PlayPauseButton paused={!isCurrentlyPlaying} />
         <div className={styles.meta}>
           <div className={styles.title}>{this.props.title}</div>
-          <div
-            className={styles.lead}
+          <div className={styles.expandable} style={isExpanded}
             dangerouslySetInnerHTML={{ __html: this.props.lead }}
           />
-          <div className={classNames(styles.lead, styles.publishedAt)}>
+          <div className={classNames(styles.publishedAt)}>
             Publisert: {publishedAt.format('DD.MM.YYYY')}
           </div>
         </div>
+        <Expander expanded={this.state.expanded} expandFunction={toggleExpanded}/>
       </div>
     );
   }
