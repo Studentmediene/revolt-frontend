d3b67c5c78e99f2a6f62ca62c601c565b69b6b41
Vemund Santi <vemund@santi.no>
2019-11-04 19:26:17 +0100

    feat: add page tracking. refactor _app.js setup

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -1,5 +1,19 @@
 import moment from 'moment';
 
+export const initMoment = () => {
+  // Set global locales for moment
+  moment.updateLocale('NB_no', {
+    calendar: {
+      lastDay: '[I går] HH:mm',
+      sameDay: '[I dag] HH:mm',
+      nextDay: '[I morgen] HH:mm',
+      sameElse: 'DD.MM.YY HH:mm',
+    },
+    weekdaysShort: 'man_tirs_ons_tors_fre_lør_søn'.split('_'),
+    weekdays: 'mandag_tirsdag_onsdag_torsdag_fredag_lørdag_søndag'.split('_'),
+  });
+};
+
 export const getNormalizedDateString = dateString => {
   const paddedString = i => (i < 10 ? `0${i}` : `${i}`);
 
