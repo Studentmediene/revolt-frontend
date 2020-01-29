a540b98ee54e42b0b40e2c55c6749edf76463002
Erik Turøy Midtun <etm.midtun@gmail.com>
2020-01-29 20:23:19 +0100

    finalised reworking the design

--- src/components/ShowList/ShowPreview/index.js
+++ src/components/ShowList/ShowPreview/index.js
@@ -7,16 +7,23 @@ import CategoryTag from 'components/common/tag/CategoryTag';
 import styles from './styles.scss';
 
 const ShowPreview = props => {
-  let categories;
+  let categories, leftBorder;
   if (props.categories && props.categories.length > 0) {
-    categories = props.categories.map((category, index) => (
-      <CategoryTag {...category} index={index} position={'top'} key={index} />
-    ));
+    categories = props.categories.map((category, index) => {
+      leftBorder = {
+        borderLeft: `${'solid 4px' +
+          props.categories.map(category => category.backgroundColor)[0]}`,
+      };
+      return (
+        <CategoryTag {...category} index={index} position={'top'} key={index} />
+      );
+    });
   }
+  // eslint-disable-next-line no-console
 
   return (
-    <div className={styles.container}>
-      {categories}
+    <div className={styles.container} style={leftBorder}>
+      <span className={styles.categoryTag}>{categories}</span>
       <Link href={`/programmer/[slug]`} as={`/programmer/${props.slug}`}>
         <a className={styles.link} href={`/programmer/${props.slug}`}>
           <div className={styles.padding}>
