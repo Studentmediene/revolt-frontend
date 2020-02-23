68c9cb8c86b508e487cfb631246006970740b6c1
Erik Turøy Midtun <etm.midtun@gmail.com>
2020-02-23 18:14:51 +0100

    all components are started on

--- src/components/Player/components/common/PlayerProgress.js
+++ src/components/Player/components/common/PlayerProgress.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import styles from './PlayerProgress.scss';
 
-const PlayerProgress = () => <div>|------------------O------|</div>;
+const PlayerProgress = () => {
+  return (
+    <div className={styles.timelineContainer}>
+      <div id={styles.timeline}>
+        <div id={styles.handle} />
+      </div>
+    </div>
+  );
+};
 
 export default PlayerProgress;
--- src/components/Player/components/common/PlayingInfo.js
+++ src/components/Player/components/common/PlayingInfo.js
@@ -2,22 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './PlayingInfo.scss';
 
-const PlayingInfo = ({ showName, episodeTitle, showImageURL }) => (
-  <div className={styles.infoContainer}>
-    <div className={styles.imgContainer}>
-      <img className={styles.img} src={showImageURL} alt={showName} />
+const PlayingInfo = ({ showName, episodeTitle, showImageURL, expand }) => (
+    <div className={styles.infoContainer} onClick={expand}>
+      <div className={styles.imgContainer}>
+        <img className={styles.img} src={showImageURL} alt={showName} />
+      </div>
+      <div className={styles.text}>
+        <h2 className={styles.showName}>{showName}</h2>
+        <h3 className={styles.episodeTitle}>{episodeTitle}</h3>
+      </div>
     </div>
-    <div className={styles.text}>
-      <h2 className={styles.showName}>{showName}</h2>
-      <h3 className={styles.episodeTitle}>{episodeTitle}</h3>
-    </div>
-  </div>
 );
 
 PlayingInfo.propTypes = {
   showName: PropTypes.string.isRequired,
   episodeTitle: PropTypes.string.isRequired,
   showImageURL: PropTypes.string.isRequired,
+  expand: PropTypes.func,
 };
 
 export default PlayingInfo;
